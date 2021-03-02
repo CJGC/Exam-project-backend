932abf2eb972cd71a5611c3c63f3bd70ae4a7212
CJG <chjugomez@utp.edu.co>
2021-03-02 13:06:54 -0500

    Removed unused imports

--- project/backend/src/main/java/co/edu/utp/isc/gia/examsapp/service/QuestionService.java
+++ project/backend/src/main/java/co/edu/utp/isc/gia/examsapp/service/QuestionService.java
@@ -11,8 +11,6 @@ import co.edu.utp.isc.gia.examsapp.validators.QuestionValidator;
 import co.edu.utp.isc.gia.examsapp.web.dto.abstractdto.QuestionDto;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
