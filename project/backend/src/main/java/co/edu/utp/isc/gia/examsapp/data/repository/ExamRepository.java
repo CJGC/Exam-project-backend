60eeb4d783f425f5579bc78d9b4a7ae37e9680e7
CJG <chjugomez@utp.edu.co>
2021-03-02 13:04:34 -0500

    Added custom method findByProfessorId to the crud repository

--- project/backend/src/main/java/co/edu/utp/isc/gia/examsapp/data/repository/ExamRepository.java
+++ project/backend/src/main/java/co/edu/utp/isc/gia/examsapp/data/repository/ExamRepository.java
@@ -6,6 +6,7 @@
 package co.edu.utp.isc.gia.examsapp.data.repository;
 
 import co.edu.utp.isc.gia.examsapp.data.entity.Exam;
+import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 
 /**
@@ -13,5 +14,5 @@ import org.springframework.data.repository.CrudRepository;
  * @author Christhian Gomez
  */
 public interface ExamRepository extends CrudRepository<Exam, Long> {
-    
+    List<Exam> findByProfessorId(Long professorID);
 }
