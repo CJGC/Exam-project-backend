38c3726cced9cce358973ad7e337b6972db8b46a
CJG <chjugomez@utp.edu.co>
2021-04-06 15:35:53 -0500

    Improved validations for professorDto

--- project/backend/src/main/java/co/edu/utp/isc/gia/examsapp/validators/ProfessorValidator.java
+++ project/backend/src/main/java/co/edu/utp/isc/gia/examsapp/validators/ProfessorValidator.java
@@ -8,97 +8,121 @@ package co.edu.utp.isc.gia.examsapp.validators;
 //import co.edu.utp.isc.gia.examsapp.web.dto.ExamDto;
 import co.edu.utp.isc.gia.examsapp.web.dto.ProfessorDto;
 import java.util.regex.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 
 /**
  *
  * @author CJ
  */
+@AllArgsConstructor
+@Setter @Getter
 public class ProfessorValidator {
     private ProfessorDto professor;
-//    private final ExamValidator examValidator;
-    
-/*    public ProfessorValidator() {
-        this.examValidator = new ExamValidator();
-    }
- */   
-    public ProfessorDto getProfessor() {
-        return professor;
-    }
-
-    public void setProfessor(ProfessorDto professor) {
-        this.professor = professor;
-    }
-    
+    private String exceptions;
+            
     public void isNull() throws Exception {
-        if (this.professor == null)
+        if (this.professor == null) {
             throw new Exception("Professor object is null");
+        }
      }
     
     public void validateId() throws Exception {
-        if (this.professor.getId() == null)
-            throw new Exception("Professor's id is null");
+        if (this.professor.getId() == null) {
+            exceptions += "Professor's id is null\n";
+            return;
+        }
+        
+        if (this.professor.getId() == 0)
+            exceptions += "Professor's id is invalid\n";
     }
 
     public void validateIdentificationCard() throws Exception {
-        if (this.professor.getIdentificationCard()== null)
-            throw new Exception("Professor's identification card is null");
-        if (Pattern.matches("", this.professor.getIdentificationCard()))
-            throw new Exception ("Professor's identification card is empty");
+        if (this.professor.getIdentificationCard()== null) {
+            exceptions += "Professor's identification card is null\n";
+            return;
+        }
+        if (Pattern.matches("", this.professor.getIdentificationCard())) {
+            exceptions += "Professor's identification card is empty\n";
+            return;
+        }
         if (!Pattern.matches("[0-9]+", this.professor.getIdentificationCard()))
-            throw new Exception("Professor's identification card is not a number");
+            exceptions += "Professor's identification card is invalid\n";
     }
     
     public void validateName() throws Exception {
-        if (this.professor.getName()== null)
-            throw new Exception("Professor's name is null");
-        if (Pattern.matches("", this.professor.getName()))
-            throw new Exception ("Professor's name is empty");
+        if (this.professor.getName()== null) {
+            exceptions += "Professor's name is null\n";
+            return;
+        }
+            
+        if (Pattern.matches("", this.professor.getName())) {
+            exceptions += "Professor's name is empty\n";
+        }
+ 
     }
 
     public void validateLastname() throws Exception {
-        if (this.professor.getLastname()== null)
-            throw new Exception("Professor's lastname is null");
-        if (Pattern.matches("", this.professor.getLastname()))
-            throw new Exception ("Professor's lastname is empty");
+        if (this.professor.getLastname()== null) {
+            exceptions += "Professor's lastname is null\n";
+            return;
+        }
+         
+        if (Pattern.matches("", this.professor.getLastname())) {
+            exceptions += "Professor's lastname is empty\n";
+        }
+        
     }
     
     public void validateEmail() throws Exception {
-        if (this.professor.getEmail()== null)
-            throw new Exception("Professor's email is null");
-        if (Pattern.matches("", this.professor.getEmail()))
-            throw new Exception ("Professor's email is empty");
+        if (this.professor.getEmail()== null) {
+            exceptions += "Professor's email is null\n";
+            return;
+        }
+            
+        if (Pattern.matches("", this.professor.getEmail())) {
+            exceptions += "Professor's email is empty\n";
+            return;
+        }
+        
         if (!Pattern.matches("[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+",
                 this.professor.getEmail()))
-            throw new Exception ("Professor's email is invalid");
+            exceptions += "Professor's email is invalid\n";
     }
     
     public void validateUsername() throws Exception {
-        if (this.professor.getUsername() == null)
-            throw new Exception("Professor's username is null");
-        if (Pattern.matches("", this.professor.getUsername()))
-            throw new Exception ("Professor's username is empty");
+        if (this.professor.getUsername() == null) {
+            exceptions += "Professor's username is null\n";
+            return;
+        }
+            
+        if (Pattern.matches("", this.professor.getUsername())) {
+            exceptions +=  "Professor's username is empty\n";
+            return;
+        }
+        
+        if (Pattern.matches("^(^[^0-9])([\\w a-z A-Z 0-9][^@#])$", this.professor.getUsername())) {
+            exceptions += "Professor's nickname is invalid\n";
+        }
     }
     
     public void validatePassword() throws Exception {
-        if (this.professor.getPassword() == null)
-            throw new Exception("Professor's password is null");
-        if (Pattern.matches("", this.professor.getPassword()))
-            throw new Exception ("Professor's password is empty");
+        if (this.professor.getPassword() == null) {
+            exceptions += "Professor's password is null\n";
+            return;
+        }
+            
+        if (Pattern.matches("", this.professor.getPassword())) {
+            exceptions += "Professor's password is empty\n";
+            return;
+        }
         if (!Pattern.matches("([a-zA-Z]){8,16}", 
                 this.professor.getPassword()))
-            throw new Exception ("Professor's password is invalid");
+            exceptions += "Professor's password is invalid\n";
     }
 
-/*    
-    public void validateExams() throws Exception {
-        if (this.professor.getExams()== null) return;
-        for (ExamDto ex : this.professor.getExams()) {
-            this.examValidator.setExam(ex);
-            this.examValidator.performValidationsExcept("id");
-        }
-    }
- */
-    public void performValidationsExcept(String attribute) throws Exception {
+    public void performValidationsExcept(String attribute) throws Exception{
         this.isNull();
         if (!attribute.equals("id")) this.validateId();
         if (!attribute.equals("identificationCard")) this.validateIdentificationCard();
@@ -107,7 +131,6 @@ public class ProfessorValidator {
         if (!attribute.equals("email")) this.validateEmail();
         if (!attribute.equals("username")) this.validateUsername();
         if (!attribute.equals("password")) this.validatePassword();
-        //if (!attribute.equals("exams")) this.validateExams();
     }
     
     public void performValidations() throws Exception {
@@ -119,6 +142,5 @@ public class ProfessorValidator {
         this.validateEmail();
         this.validateUsername();
         this.validatePassword();
-        //this.validateExams();
     }
 }
